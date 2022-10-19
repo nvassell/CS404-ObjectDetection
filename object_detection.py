b84f37f81cd28cb35969df105a54a808559a5e51
Nathan Vassell <vass1881@vandals.uidaho.edu>
2022-10-19 11:08:16 -0700

    Update identification methods

--- object_detection.py
+++ object_detection.py
@@ -1,19 +1,17 @@
 import cv2 as cv
+from cv2 import drawKeypoints
+
 import numpy as np
-
-
 
 def SIFT():
     img = cv.imread("src/ball1.jpg")
-    key = ord('r')
-    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    imggray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     sift = cv.SIFT_create()
     # Secont parameter is option mask to pass function
-    kp = sift.detect(img, None)
+    kp = sift.detect(imggray, None)
     img = drawKeypoints(img, kp, outImage=None)
     cv.imshow("image", img)
-    while key != ord('s'):
-        key = cv.waitKey(5);
+    cv.waitKey();
 
 def BRISK():
     img = cv.imread("src/ball1.jpg")
@@ -40,7 +38,6 @@ def FAST():
         key = cv.waitKey(5);
 
 def blob():
-    key = ord('r')
     origimg = cv.imread("src/ball1.jpg")
     img = cv.cvtColor(origimg, cv.COLOR_BGR2GRAY)
     blob = cv.SimpleBlobDetector_create()
@@ -48,8 +45,7 @@ def blob():
     kp = blob.detect(img, None)
     img = drawKeypoints(origimg, kp, outImage=None, color=(0,0,225), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     cv.imshow("image", img)
-    while key != ord('s'):
-        key = cv.waitKey(5);\
+    cv.waitKey()
 
 def Harris():
     img = cv.imread("src/ball1.jpg")
@@ -66,25 +62,31 @@ def Harris():
     cv.waitKey()
 
 def Orb():
-    img1 = cv.imread("src/ball3.jpg", 0)
-    img2 = cv.imread("src/ball2.jpg", 0)
-
+    img = cv.imread("src/ball1.jpg")
     orb = cv.ORB_create(nfeatures=500)
-    kp1, des1 = orb.detectAndCompute(img1, None)
-    kp2, des2 = orb.detectAndCompute(img2, None)
+    kp, des = orb.detectAndCompute(img, None)
+    cv.imshow()
 
-    # matcher takes normType, which is set to cv2.NORM_L2 for SIFT and SURF, cv2.NORM_HAMMING for ORB, FAST and BRIEF
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1, des2)
-    matches = sorted(matches, key=lambda x: x.distance)
-    # draw first 50 matches
-    match_img = cv.drawMatches(img1, kp1, img2, kp2, matches[:50], None)
-    cv.imshow('Matches', match_img)
-    cv.waitKey()
 
-# SIFT()
+    # img1 = cv.imread("src/ball3.jpg", 0)
+    # img2 = cv.imread("src/ball2.jpg", 0)
+
+    # orb = cv.ORB_create(nfeatures=500)
+    # kp1, des1 = orb.detectAndCompute(img1, None)
+    # kp2, des2 = orb.detectAndCompute(img2, None)
+
+    # # matcher takes normType, which is set to cv2.NORM_L2 for SIFT and SURF, cv2.NORM_HAMMING for ORB, FAST and BRIEF
+    # bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    # matches = bf.match(des1, des2)
+    # matches = sorted(matches, key=lambda x: x.distance)
+    # # draw first 50 matches
+    # match_img = cv.drawMatches(img1, kp1, img2, kp2, matches[:50], None)
+    # cv.imshow('Matches', match_img)
+    # cv.waitKey()
+
+SIFT()
 # BRISK()
 # FAST()
 # blob()
 # Harris()
-Orb()
+# Orb()
